81902839280f8f49cc64bda136ee27d58441c979
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 19:39:56 +0300

    0

--- src/Main/Initilizator.lua
+++ src/Main/Initilizator.lua
@@ -6,26 +6,7 @@ local RenderUtil = loadstring(game:HttpGet(
         'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Util/Render/URender2D.lua'
 ))()
 
-if _G.__G_injected then
-    UChat.chat.sendMessage("Скрипт уже загружен!")
-    return
-end
-_G.__G_injected = true
+local version = 0.01
 
+UChat.sendMessage('Скрипт Загружен | Версия ' .. version)
 RenderUtil:drawRoundedRectangle(100, 100, 300, 150, 20, Color3.fromRGB(255,255,255), 0.25)
-
-local function uninject()
-    UChat.chat.sendMessage('Uninjecting...')
-    if RenderUtil and RenderUtil.Clear then
-        RenderUtil:Clear()
-    end
-    _G.__G_injected = false
-    UChat.chat.sendMessage('All cleaned!')
-end
-
-game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
-    if gameProcessed then return end
-    if input.KeyCode == Enum.KeyCode.Equals then
-        uninject()
-    end
-end)
