a48597b2f3e515e8da271186bfcd442c90edfc14
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 15:29:24 +0300

    1

--- src/main/Initilizator.lua
+++ src/main/Initilizator.lua
@@ -1,4 +1,12 @@
-local chunk = loadstring(game:HttpGet('https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua'))()
+
+local chunk = loadstring(game:HttpGet('https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua'))
+if not chunk then
+    error("Не удалось скомпилировать Util.lua")
+end
+
 local Util = chunk()
+if not Util then
+    error("Util.lua вернул nil")
+end
 
 Util.chat.sendMessage("Hello World!!!")
