0a42df42312f3012d7ec81945513ade3bd5ea44a
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 20:36:50 +0300

    newtest
    wwwwww

--- src/Main/Module/Updater/EveryUpdater.lua
+++ src/Main/Module/Updater/EveryUpdater.lua
@@ -6,26 +6,27 @@ local player = Players.LocalPlayer
 local EveryUpdater = {}
 EveryUpdater.__index = EveryUpdater
 
-function EveryUpdater:new()
+EveryUpdater.get = {}
+
+function EveryUpdater.get.new()
     local self = setmetatable({}, EveryUpdater)
     self:_initKeybinds()
     return self
 end
 
-function EveryUpdater:_initKeybinds()
+function EveryUpdater.get._initKeybinds()
     UserInputService.InputBegan:Connect(function(input, gameProcessed)
         if gameProcessed then return end
         if input.KeyCode == Enum.KeyCode.Equals then
-            self:TeleportToSameServer()
+            self.get.TeleportToSameServer()
         end
     end)
 end
 
-function EveryUpdater:TeleportToSameServer()
+function EveryUpdater.get.TeleportToSameServer()
     local jobId = game.JobId
     local placeId = game.PlaceId
     print("[EveryUpdater] Teleporting to current server...")
-    TeleportService:TeleportToPlaceInstance(placeId, jobId, player)
+    TeleportService.get.TeleportToPlaceInstance(placeId, jobId, player)
 end
 
-local updater = EveryUpdater:new()
