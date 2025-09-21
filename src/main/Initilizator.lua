c9197662bcea298797a26da1835fa124fcf5fc86
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 15:24:42 +0300

    1

--- src/main/Initilizator.lua
+++ src/main/Initilizator.lua
@@ -1,12 +1,4 @@
-local http = require("socket.http")
-local url = "https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua"
-
-local body, code = http.request(url)
-if not body then
-    error("Не удалось загрузить скрипт: " .. tostring(code))
-end
-
-local chunk = loadstring(body)
+local chunk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua"))()
 local Util = chunk()
 
 Util.chat.sendMessage("Hello World!!!")
