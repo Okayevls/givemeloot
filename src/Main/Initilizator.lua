e0c3b13d690dd7adebb11237ae5fc842cc8b3f91
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 19:23:28 +0300

    1

--- src/Main/Initilizator.lua
+++ src/Main/Initilizator.lua
@@ -1,19 +1,18 @@
+local getRep = loadstring(game:HttpGet('https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Util/Chat/UChat.lua'))
+local loadRender = loadstring(game:HttpGet('https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Util/Render/URender2D.lua'))
+
+loadRender():drawRoundedRectangle(100, 100, 200, 100, true, Color3.fromRGB(255,0,0))
+
 if _G.__G_injected then
     getRep().sendMessage("Скрипт уже загружен!")
     return
 end
 _G.__G_injected = true
 
-local getRep = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/'
-local loadRender = loadstring(game:HttpGet(getRep .. 'Util/Render/URender2D.lua'))
-local RenderUtil = loadRender()
-
-RenderUtil:drawRoundedRectangle(100, 100, 200, 100, true, Color3.fromRGB(255,0,0))
-
 local function uninject()
     getRep().sendMessage('Uninjecting...')
-    if RenderUtil and RenderUtil.Clear then
-        RenderUtil:Clear()
+    if loadRender() and loadRender().Clear then
+        loadRender():Clear()
     end
     _G.__G_injected = false
     getRep().sendMessage('All cleaned!')
