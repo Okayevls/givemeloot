5ae456ce75273fd261eb54728b9fea6f6d24b46c
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 20:27:53 +0300

    5

--- src/Main/Module/ModuleInitilizator.lua
+++ src/Main/Module/ModuleInitilizator.lua
@@ -1,10 +1,6 @@
 local ModuleInitilizator = {}
 ModuleInitilizator.__index = ModuleInitilizator
 
-local EveryUpdaterModule = loadstring(game:HttpGet(
-        'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Module/Updater/EveryUpdater.lua'
-))()
-
 function ModuleInitilizator:new()
     print("111111")
 end
