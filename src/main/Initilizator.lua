00dafd119d6b1e2ac6185af92ef05c97b7de050e
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 15:26:16 +0300

    2

--- src/main/Initilizator.lua
+++ src/main/Initilizator.lua
@@ -1,4 +1,4 @@
-local chunk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua"))()
+local chunk = loadstring(game:HttpGet('https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/Util.lua'))()
 local Util = chunk()
 
 Util.chat.sendMessage("Hello World!!!")
