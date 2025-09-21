6408c5152d3e9f19fb86584ef75e2a107020c3d4
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 19:41:46 +0300

    1

--- src/Main/Initilizator.lua
+++ src/Main/Initilizator.lua
@@ -8,5 +8,5 @@ local RenderUtil = loadstring(game:HttpGet(
 
 local version = 0.01
 
-UChat().sendMessage('Скрипт Загружен | Версия ' .. version)
+UChat.chat.sendMessage('Скрипт Загружен | Версия ' .. version)
 RenderUtil:drawRoundedRectangle(100, 100, 300, 150, 20, Color3.fromRGB(255,255,255), 0.25)
