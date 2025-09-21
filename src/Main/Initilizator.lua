209f29f1c23439f84a2cfcd9ef29472ac4f156ad
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-21 19:05:46 +0300

    new class

---
+++ src/Main/Initilizator.lua
@@ -0,0 +1,3 @@
+local getRep = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/'
+local loadUtil = loadstring(game:HttpGet(getRep + 'util/chat/UChat.lua'))
+loadUtil().chat.sendMessage("Hello World!!!")
--- src/main/Initilizator.lua
+++
@@ -1,2 +0,0 @@
-local loadUtil = loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/main/util/chat/UChat.lua"))
-loadUtil().chat.sendMessage("Hello World!!!")
