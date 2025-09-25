755f2c842044deb18698f4ba5ea7435b64ffd2db
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-25 21:05:57 +0300

    Auto-commit 25.09.2025 21:05:56,66

--- src/Main/Main.lua
+++ src/Main/Main.lua
@@ -7,7 +7,12 @@ local modules = {
     FontRender  = "https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Util/Render/UFontRenderer.lua",
 
     Updater = "https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Module/Update/Updater.lua",
-    GuiRenderer = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Gui/GuiRenderer.lua'
+
+    GuiRenderer = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Gui/GuiRenderer.lua',
+    GuiThemeManager = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Gui/ThemeManager.lua',
+    GuiSaveManager = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Gui/SaveManager.lua',
+
+    Category = 'https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Module/Category.lua'
 }
 
 EventLoader:Init(modules)
@@ -16,14 +21,18 @@ local Chat = EventLoader:Get("Chat")
 local Updater = EventLoader:Get("Updater")
 local Render = EventLoader:Get("Render")
 local FontRender = EventLoader:Get("FontRender")
+
 local GuiRenderer = EventLoader:Get("GuiRenderer")
+local Category = EventLoader:Get("Category")
+local GuiThemeManager = EventLoader:Get("ThemeManager")
+local GuiSaveManager = EventLoader:Get("SaveManager")
 
 local updaterReconnect = Updater:new()
 
 local Window = GuiRenderer:CreateWindow({ Title = '                     $ ClosedPvP.lucky | Beta $                     ', AutoShow = true, TabPadding = 15, MenuFadeTime = 0.2 })
+Category:drawCategory(Window)
 
 Chat.chat.sendMessage("Created by Prokosik x Flyaga other sucked dick")
-Chat.chat.sendMessage("Build Test 000000012")
 
 
 
---
+++ src/Main/Module/Category.lua
@@ -0,0 +1,15 @@
+local Category = {}
+Category.__index = Category
+
+Category.type = {}
+
+function Category:drawCategory(window)
+    local Tabs = { Main = Window:AddTab('Main'),
+                   Character = Window:AddTab('Character'),
+                   Visuals = Window:AddTab('Visuals'),
+                   Misc = Window:AddTab('Misc'),
+                   Players = Window:AddTab('Players'),
+                   ['UI Settings'] = Window:AddTab('UI Settings') }
+end
+
+return Category
