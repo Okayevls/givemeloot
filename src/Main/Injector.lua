31616d55ba7d589e0767a17211e602472c2d4dff
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-25 13:14:54 +0300

    1

--- src/Main/Injector.lua
+++ src/Main/Injector.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/givemeloot/refs/heads/main/src/Main/Main.lua"))()
+local branch = "main"
+local repoUser = "Okayevls"
+local repoName = "givemeloot"
+
+local apiUrl = "https://api.github.com/repos/"..repoUser.."/"..repoName.."/commits/"..branch
+local data = game:GetService("HttpService"):JSONDecode(game:HttpGet(apiUrl))
+local latestSHA = data["sha"]
+
+local rawUrl = "https://raw.githubusercontent.com/"..repoUser.."/"..repoName.."/"..latestSHA.."/src/Main/Main.lua?v="..os.time()
+loadstring(game:HttpGet(rawUrl))()
