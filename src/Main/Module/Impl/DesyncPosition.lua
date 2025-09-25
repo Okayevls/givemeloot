a2db05b1dde2d594253083505934ff3a968effd5
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-25 23:56:54 +0300

    Auto-commit 25.09.2025 23:56:53,92

--- src/Main/Module/Impl/DesyncPosition.lua
+++ src/Main/Module/Impl/DesyncPosition.lua
@@ -5,17 +5,17 @@ DesyncPosition.type = {}
 
 function DesyncPosition:drawModule(Tabs)
     local playerGroup = Tabs.Character:AddLeftGroupbox('Player Position')
-    --playerGroup:AddToggle('desyncposition_toggle', {
-    --    Text = 'Desync Position',
-    --    Default = false,
-    --    Callback = function(state)
-    --        if state then
+    playerGroup:AddToggle('desyncposition_toggle', {
+        Text = 'Desync Position',
+        Default = false,
+        Callback = function(state)
+            if state then
 --
-    --        else
+            else
 --
-    --        end
-    --    end
-    --})
+            end
+        end
+    })
     return playerGroup
 end
 
