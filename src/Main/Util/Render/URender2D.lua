f3ec4b5baf536059f68620474655fe4cbbf23a3d
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-25 14:34:08 +0300

    Auto-commit 25.09.2025 14:34:08,08

--- src/Main/Util/Render/URender2D.lua
+++ src/Main/Util/Render/URender2D.lua
@@ -1,20 +1,28 @@
 local player = game.Players.LocalPlayer
 local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "DevRenderGui"
 screenGui.Parent = player:WaitForChild("PlayerGui")
 
 local RenderUtil = {}
 RenderUtil.__index = RenderUtil
-
 RenderUtil.shapes = {}
+
+local function ensureColor3(color)
+    if typeof(color) == "Color3" then
+        return color
+    else
+        return Color3.fromRGB(255,255,255)
+    end
+end
 
 function RenderUtil:drawRoundedRectangle(x, y, width, height, cornerRadius, color, alpha)
     local frame = Instance.new("Frame")
     frame.Size = UDim2.new(0, width or 100, 0, height or 50)
     frame.Position = UDim2.new(0, x or 0, 0, y or 0)
-    frame.BackgroundColor3 = color or Color3.fromRGB(255, 255, 255)
+    frame.BackgroundColor3 = ensureColor3(color)
     frame.BackgroundTransparency = alpha or 0
     frame.BorderSizePixel = 0
-    frame.Parent = screenGui.Parent
+    frame.Parent = screenGui
 
     local uiCorner = Instance.new("UICorner")
     uiCorner.CornerRadius = UDim.new(0, cornerRadius or 10)
