01bc447ff5e2f2138a4c96798cf19d3015ace185
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-26 11:16:31 +0300

    Auto-commit 26.09.2025 11:16:31,65

--- src/Main/Main.lua
+++ src/Main/Main.lua
@@ -26,6 +26,23 @@ local updaterReconnect = Updater:new()
 
 local Window = GuiRenderer.new("ClosedPvP UI", "v0.0.1", 4370345701)
 
+local Tab1 = Window.Tab("Tab 1", 6026568198)
+local Folder = Tab1.Folder("Options", "A bunch of options you can use")
+Folder.Button("Button", function()
+    print("Button Clicked")
+end)
+Folder.Switch("Switch", function(Status)
+    print("Switch Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+    print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.TextBox("Textbox", "Placeholder", function(Text)
+    print("TextBox Triggered: " .. Text)
+end)
+Tab1.Folder("ClosedPvP Expanded", "Test this is govnishe.")
+
+
 Chat.chat.sendMessage("Created by Prokosik x Flyaga other sucked dick")
 Chat.chat.sendMessage("Build to loading 000000002")
 
