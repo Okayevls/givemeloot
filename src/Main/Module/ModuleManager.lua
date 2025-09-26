a8a58e4431a4e8179643ff3a17ce6474e8fb4c24
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-26 13:12:59 +0300

    Auto-commit 26.09.2025 13:12:58,96

--- src/Main/Module/ModuleManager.lua
+++ src/Main/Module/ModuleManager.lua
@@ -1,11 +1,15 @@
--- Утилита для загрузки скриптов с GitHub
 local function LoadGitHubScript(user, repo, path, branch)
     branch = branch or "main"
+    local httpService = game:GetService("HttpService")
+
     local apiUrl = "https://api.github.com/repos/"..user.."/"..repo.."/commits/"..branch
-    local latestSHA = game:GetService("HttpService"):JSONDecode(game:HttpGet(apiUrl))["sha"]
-    local rawUrl = "https://raw.githubusercontent.com/"..user.."/"..repo.."/"..latestSHA.."/"..path.."?v="..os.time()
-    return loadstring(game:HttpGet(rawUrl))()
+    local data = httpService:JSONDecode(game:HttpGet(apiUrl, true))
+    local latestSHA = data["sha"]
+
+    local rawUrl = "https://raw.githubusercontent.com/"..user.."/"..repo.."/"..latestSHA.."/"..path
+    return game:HttpGet(rawUrl, true)
 end
+
 
 local ModuleLoader = {}
 ModuleLoader.__index = ModuleLoader
@@ -17,19 +21,22 @@ ModuleLoader.modules = {
 function ModuleLoader:loadEvent()
     self.loadedModules = {}
     for name, path in pairs(self.modules) do
-        local success, module = pcall(LoadGitHubScript, "Okayevls", "givemeloot", path)
+        local success, result = pcall(function()
+            local code = LoadGitHubScript("Okayevls", "givemeloot", path)
+            return loadstring(code)()
+        end)
         if success then
-            self.loadedModules[name] = module
+            self.loadedModules[name] = result
         else
-            warn("[ModuleLoader] Error loading module:", name, module)
+            warn("[ModuleLoader] X Error loading module:", name, result)
         end
     end
 end
 
 function ModuleLoader:drawMainModule(MainTab)
-    local desyncModule = self.loadedModules.DesyncPosition
-    if desyncModule and desyncModule.drawModule then
-        desyncModule:drawModule(MainTab)
+    local module = self.loadedModules.DesyncPosition
+    if module and module.drawModule then
+        module:drawModule(MainTab)
     else
         warn("[ModuleLoader] Module 'DesyncPosition' not loaded or missing drawModule function")
     end
