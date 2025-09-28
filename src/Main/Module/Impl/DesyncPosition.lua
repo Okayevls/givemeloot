464926b1c3b47f795dad296c645f945454c3c1ed
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-28 23:03:46 +0300

    Auto-commit 28.09.2025 23:03:46,13

--- src/Main/Module/Impl/DesyncPosition.lua
+++ src/Main/Module/Impl/DesyncPosition.lua
@@ -1,30 +1,14 @@
 local DesyncPosition = {}
 DesyncPosition.__index = DesyncPosition
 
-function DesyncPosition:drawModule(MainTab)
-    local Folder = MainTab:Folder("Position Player", "[Info] Server position desync")
+DesyncPosition.type = {}
 
-   --Folder:SwitchAndBinding("Position Desync", function(state)
-   --    if state then
-   --    else
-   --    end
-   --end)
+function DesyncPosition:drawModule(MainTab)
+    local Folder = MainTab.Folder("Position Player", "[Info] Controlling player server position")
+
+    Folder.SwitchAndBinding("Position Desync", function(Status)
+        print("Switch Triggered: " .. tostring(Status))
+    end)
 end
 
 return DesyncPosition
-
-
---local DesyncPosition = {}
---DesyncPosition.__index = DesyncPosition
---
---DesyncPosition.type = {}
---
---function DesyncPosition:drawModule(MainTab)
---    local Folder = MainTab.Folder("Position Player", "[Info] Controlling player server position")
---
---    Folder.SwitchAndBinding("Position Desync", function(Status)
---        print("Switch Triggered: " .. tostring(Status))
---    end)
---end
---
---return DesyncPosition
