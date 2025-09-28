d4e3630a641a61d9721f37ab8eb63285ca2eb93f
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-28 23:52:37 +0300

    Auto-commit 28.09.2025 23:52:36,73

--- src/Main/Gui/GuiInstance.lua
+++ src/Main/Gui/GuiInstance.lua
@@ -4,7 +4,7 @@ GuiInstance.__index = GuiInstance
 GuiInstance.type = {}
 
 function GuiInstance:drawGuiCore(GuiRenderer, ModuleManager)
-    local Window = GuiRenderer.new("Morphis", "vBeta I", 4370345701)
+    local Window = GuiRenderer.new("Morphis", "Anomic - vBeta I", 4370345701)
     local MainTab = Window.Tab("Character", 6026568198)
 
     ModuleManager:loadEvent()
