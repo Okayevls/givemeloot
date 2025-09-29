c3ea05c0b4399e5568904ea22d5384826f412baa
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-29 15:15:07 +0300

    Auto-commit 29.09.2025 15:15:07,01

--- src/Main/Gui/GuiInstance.lua
+++ src/Main/Gui/GuiInstance.lua
@@ -9,7 +9,7 @@ function GuiInstance:drawGuiCore(ModuleLoader, GuiRenderer, ModuleManager)
     local CombatTab = Window.Tab("Combat", 7485051733)
     local CharacterTab = Window.Tab("Character", 16149111790)
 
-    print("111111111")
+    print("0000")
 
     local drawCategoryCombat = ModuleManager:drawCategory(ModuleLoader,"Combat", CombatTab)
     local drawCategoryCharacter = ModuleManager:drawCategory(ModuleLoader,"Character", CharacterTab)
