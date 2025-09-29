6dc02c9080821ea8e75846d7099d7476e4637ffc
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-29 10:54:41 +0300

    Auto-commit 29.09.2025 10:54:41,19

--- src/Main/Module/Impl/AimBot.lua
+++ src/Main/Module/Impl/AimBot.lua
@@ -4,16 +4,10 @@ AimBot.__index = AimBot
 AimBot.type = {}
 
 function AimBot:drawModule(MainTab)
-    local Folder = MainTab:Folder("AimBot", "[Info] Automatically finds the target and destroys it")
+    local Folder = MainTab.Folder("AimBot", "[Info] Automatically finds the target and destroys it")
 
-    Folder:SwitchAndBinding("Enable AimBot", function(Status)
-        print("[AimBot] Enabled:", Status)
-        self.Enabled = Status
-    end)
+    Folder.Switch   ("SilentAim", function(Status)
 
-    Folder:Slider("Aimbot FOV", 10, 360, 90, function(Value)
-        print("[AimBot] FOV:", Value)
-        self.FOV = Value
     end)
 end
 
