19c3fa07808c54710aa13b25489bba3b594e8953
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-29 12:21:59 +0300

    Auto-commit 29.09.2025 12:21:59,30

--- src/Main/Gui/GuiInstance.lua
+++ src/Main/Gui/GuiInstance.lua
@@ -4,7 +4,7 @@ GuiInstance.__index = GuiInstance
 GuiInstance.type = {}
 
 function GuiInstance:drawGuiCore(ModuleLoader, GuiRenderer, ModuleManager)
-    local Window = GuiRenderer.new("Morphis", "TB3 - vBeta I", 4370345701)
+    local Window = GuiRenderer.new("Morphis", "Anomic - vBeta I", 4370345701)
 
     local CombatTab = Window.Tab("Combat", 7485051733)
     local CharacterTab = Window.Tab("Character", 16149111790)
--- src/Main/Module/ModuleManager.lua
+++ src/Main/Module/ModuleManager.lua
@@ -1,45 +1,26 @@
 local ModuleManager = {}
 ModuleManager.__index = ModuleManager
 
-function ModuleManager.new()
-    local self = setmetatable({}, ModuleManager)
-    return self
-end
+function ModuleManager:drawCategory(ModuleLoader, Category, MainTab)
+    local loader = ModuleLoader
 
-function ModuleManager:LoadAndDrawCategory(ModuleLoader, category, MainTab)
-    assert(ModuleLoader, "[ModuleManager] ModuleLoader required")
-
-    local modules = {}
-    if category == "Combat" then
-        modules = {
+    loader:Init({
+        Combat = {
             Aimbot = "src/Main/Module/Impl/Aimbot.lua"
-        }
-    elseif category == "Character" then
-        modules = {
+        },
+        Character = {
             DesyncPosition = "src/Main/Module/Impl/DesyncPosition.lua"
         }
-    else
-        warn("[ModuleManager] Unknown category:", category)
-        return
+    })
+
+    if MainTab == "Combat" then
+        local Aimbot = loader:Get("Aimbot"):drawModule(MainTab)
     end
 
-    ModuleLoader:Init({ [category] = modules })
-
-    for name, _ in pairs(modules) do
-        local success, mod = pcall(function()
-            return ModuleLoader:Get(category, name)
-        end)
-        if success and mod and type(mod.drawModule) == "function" then
-            local ok, err = pcall(function()
-                mod:drawModule(MainTab)
-            end)
-            if not ok then
-                warn(("[ModuleManager] ❌ Failed to draw module '%s' in '%s': %s"):format(name, category, err))
-            end
-        else
-            warn(("[ModuleManager] ⚠️ Module '%s' in '%s' missing or invalid"):format(name, category))
-        end
+    if MainTab == "Character" then
+        local Desync = loader:Get("DesyncPosition"):drawModule(MainTab)
     end
+
 end
 
 return ModuleManager
