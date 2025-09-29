3a85f8922e8a44a929ccc6f07f210060094ea579
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-30 01:18:02 +0300

    Auto-commit 30.09.2025  1:18:01,70

--- src/Main/Module/ModuleManager.lua
+++ src/Main/Module/ModuleManager.lua
@@ -7,13 +7,16 @@ function ModuleManager:drawCategory(Window, ModuleLoader)
     loader:Init({
         AimBot = "src/Main/Module/Impl/AimBot.lua",
         DesyncPosition = "src/Main/Module/Impl/DesyncPosition.lua"
+        DesyncPosition = "src/Main/Module/Impl/Speed.lua"
     })
 
     local CombatTab = Window.Tab("Combat", 7485051733)
     local CharacterTab = Window.Tab("Character", 16149111790)
 
     local Aimbot = loader:Get("AimBot"):drawModule(CombatTab)
+
     local DesyncPosition = loader:Get("DesyncPosition"):drawModule(CharacterTab)
+    local Speed = loader:Get("Speed"):drawModule(CharacterTab)
 
 end
 
