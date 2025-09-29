2c0ea48513da426651864f66e28048e994f16331
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-30 00:04:44 +0300

    Auto-commit 30.09.2025  0:04:44,18

--- src/Main/Module/ModuleManager.lua
+++ src/Main/Module/ModuleManager.lua
@@ -5,27 +5,16 @@ function ModuleManager:drawCategory(Window, ModuleLoader)
     local loader = ModuleLoader
 
     loader:Init({
-        Aimbot = "src/Main/Module/Impl/AimBot.lua",
+        AimBot = "src/Main/Module/Impl/AimBot.lua",
         DesyncPosition = "src/Main/Module/Impl/DesyncPosition.lua"
     })
 
     local CombatTab = Window.Tab("Combat", 7485051733)
     local CharacterTab = Window.Tab("Character", 16149111790)
 
-    local Aimbot = loader:Get("Aimbot")
-    if Aimbot then
-        Aimbot:drawModule(CombatTab)
-    else
-        warn("[ModuleManager] ❌ Aimbot module is nil!")
-    end
+    local Aimbot = loader:Get("AimBot"):drawModule(CombatTab)
+    local DesyncPosition = loader:Get("DesyncPosition"):drawModule(CharacterTab)
 
-    local DesyncPosition = loader:Get("DesyncPosition")
-    if DesyncPosition then
-        DesyncPosition:drawModule(CharacterTab)
-    else
-        warn("[ModuleManager] ❌ DesyncPosition module is nil!")
-    end
 end
-
 
 return ModuleManager
