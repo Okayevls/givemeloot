b8571c420a71dcfd1dafc3bb6a17fcc24d42440d
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-30 12:43:52 +0300

    Auto-commit 30.09.2025 12:43:52,59

--- src/Main/Gui/GuiInstance.lua
+++ src/Main/Gui/GuiInstance.lua
@@ -6,16 +6,9 @@ GuiInstance.type = {}
 function GuiInstance:drawGuiCore(ModuleLoader, GuiRenderer, ModuleManager)
     local Window = GuiRenderer.new("Morphis", "Anomic - vBeta I", 4370345701)
 
-    local UserInputService = game:GetService("UserInputService")
-    local debounce = false
-
-    UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
-        if gameProcessedEvent then return end
-        if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.RightShift then
-            if debounce then return end
-            debounce = true
+    game:GetService("UserInputService").InputBegan:Connect(function(Input)
+        if Input.KeyCode == Enum.KeyCode.M then
             Window:Toggle()
-            delay(0.2, function() debounce = false end)
         end
     end)
 
