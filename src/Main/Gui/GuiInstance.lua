965bd8b51b00b9810dd346bce2387398070592a2
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-09-30 12:31:05 +0300

    Auto-commit 30.09.2025 12:31:04,70

--- src/Main/Gui/GuiInstance.lua
+++ src/Main/Gui/GuiInstance.lua
@@ -6,6 +6,12 @@ GuiInstance.type = {}
 function GuiInstance:drawGuiCore(ModuleLoader, GuiRenderer, ModuleManager)
     local Window = GuiRenderer.new("Morphis", "Anomic - vBeta I", 4370345701)
 
+    game:GetService("UserInputService").InputBegan:Connect(function(Input)
+        if Input.KeyCode == Enum.KeyCode.RSHIFT then
+            Window:Toggle()
+        end
+    end)
+
     ModuleManager:drawCategory(Window, ModuleLoader)
 end
 
