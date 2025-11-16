4fa0f83486dfc1f88716d74bb911a41812830ba8
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-11-17 00:42:47 +0200

    Auto-commit 17.11.2025  0:42:47,46

--- src/Main/Module/ModuleManager.lua
+++ src/Main/Module/ModuleManager.lua
@@ -9,11 +9,13 @@ function ModuleManager:drawCategory(Window, ModuleLoader)
         Fly = "src/Main/Module/Impl/Fly.lua",
         ChatSpy = "src/Main/Module/Impl/ChatSpy.lua",
         AutoRedeem = "src/Main/Module/Impl/AutoRedeem.lua",
+        Esp = "src/Main/Module/Impl/Esp.lua",
         Speed = "src/Main/Module/Impl/Speed.lua"
     })
 
     local CombatTab = Window.Tab("Combat", 7485051733)
     local CharacterTab = Window.Tab("Character", 16149111790)
+    local VisualTab = Window.Tab("Visuals", 16149111790)
     local OtherTab = Window.Tab("Other", 16149111790)
 
     local SilentAim = loader:Get("SilentAim"):drawModule(CombatTab)
@@ -21,10 +23,12 @@ function ModuleManager:drawCategory(Window, ModuleLoader)
     local Speed = loader:Get("Speed"):drawModule(CharacterTab)
     local Fly = loader:Get("Fly"):drawModule(CharacterTab)
 
+    local Esp = loader:Get("Esp"):drawModule(VisualTab)
+
     local ChatSpy = loader:Get("ChatSpy"):drawModule(OtherTab)
     local AutoRedeem = loader:Get("AutoRedeem"):drawModule(OtherTab)
 
-    print("Base ModuleManager Build | 0x000000000117")
+    print("Base ModuleManager Build | 0x000000000118")
 
 end
 
