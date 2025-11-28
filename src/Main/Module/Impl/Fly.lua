cbaef0055887ac5b35f5c4545597e39e099e3357
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-11-28 23:40:30 +0200

    Auto-commit 28.11.2025 23:40:29,66

--- src/Main/Module/Impl/Fly.lua
+++ src/Main/Module/Impl/Fly.lua
@@ -95,7 +95,7 @@ function Fly:drawModule(MainTab, Notifier)
         end
     end)
 
-    Folder.Slider("Fly Speed", { Min = 0, Max = 5, Default = 1, Step = 0.01 }, function(value)
+    Folder.Slider("Fly Speed", { Min = 0, Max = 500, Default = 1, Step = 0.01 }, function(value)
         Fly.FlyMultiplier = value
     end)
 
