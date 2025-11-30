2ba612e37555068625b5a65f994bda7f26ac1c64
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-11-30 15:54:41 +0200

    Auto-commit 30.11.2025 15:54:41,18

--- src/Main/Module/Impl/JumpCircle.lua
+++ src/Main/Module/Impl/JumpCircle.lua
@@ -15,10 +15,6 @@ local rootPart = character:WaitForChild("HumanoidRootPart")
 local RunService = game:GetService("RunService")
 
 local IMAGE_ID = "rbxassetid://13585653697"
-local START_RADIUS = 1
-local END_RADIUS = 8
-local HEIGHT = 0.05
-local FADE_TIME = 0.52
 local OFFSET_DOWN = 1.5
 
 local function createCircle()
@@ -26,8 +22,8 @@ local function createCircle()
     part.Anchored = true
     part.CanCollide = false
     part.CastShadow = false
-    part.Size = Vector3.new(START_RADIUS*2, HEIGHT, START_RADIUS*2)
-    part.CFrame = rootPart.CFrame - Vector3.new(0, rootPart.Size.Y/2 + HEIGHT/2 + OFFSET_DOWN, 0)
+    part.Size = Vector3.new(JumpCircle.START_RADIUS *2, JumpCircle.HEIGHT, JumpCircle.START_RADIUS*2)
+    part.CFrame = rootPart.CFrame - Vector3.new(0, rootPart.Size.Y/2 + JumpCircle.HEIGHT/2 + OFFSET_DOWN, 0)
     part.Transparency = 1
     part.Material = Enum.Material.Neon
     part.Parent = workspace
@@ -51,13 +47,13 @@ local function createCircle()
     local connection
     connection = RunService.Heartbeat:Connect(function()
         local elapsed = tick() - startTime
-        if elapsed >= FADE_TIME then
+        if elapsed >= JumpCircle.FADE_TIME then
             part:Destroy()
             connection:Disconnect()
         else
-            local t = elapsed / FADE_TIME
-            local radius = START_RADIUS + (END_RADIUS - START_RADIUS) * t
-            part.Size = Vector3.new(radius*2, HEIGHT, radius*2)
+            local t = elapsed / JumpCircle.FADE_TIME
+            local radius = JumpCircle.START_RADIUS + (JumpCircle.END_RADIUS - JumpCircle.START_RADIUS) * t
+            part.Size = Vector3.new(radius*2, JumpCircle.HEIGHT, radius*2)
             image.ImageTransparency = t
         end
     end)
