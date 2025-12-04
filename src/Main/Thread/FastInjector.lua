fe1f3e3e14ac32876fe8cd60cc8a2863f413ae35
Okayevls <151445860+Okayevls@users.noreply.github.com>
2025-12-04 19:07:24 +0200

    Auto-commit 04.12.2025 19:07:24,21

--- src/Main/Thread/FastInjector.lua
+++ src/Main/Thread/FastInjector.lua
@@ -1,8 +1,10 @@
-print("111111")
+local branch = "main"
+local repoUser = "Okayevls"
+local repoName = "givemeloot"
 
-local apiUrl = "https://api.github.com/repos/Okayevls/givemeloot/commits/main"
+local apiUrl = "https://api.github.com/repos/"..repoUser.."/"..repoName.."/commits/"..branch
 local data = game:GetService("HttpService"):JSONDecode(game:HttpGet(apiUrl))
 local latestSHA = data["sha"]
 
-local rawUrl = "https://raw.githubusercontent.com/Okayevls/givemeloot/"..latestSHA.."/src/Main/Main.lua?v="..os.time()
+local rawUrl = "https://raw.githubusercontent.com/"..repoUser.."/"..repoName.."/"..latestSHA.."/src/Main/Main.lua?v="..os.time()
 loadstring(game:HttpGet(rawUrl))()
